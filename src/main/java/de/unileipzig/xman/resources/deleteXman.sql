93cd6be306bd6a30a60aed9a9c47e46b7ad7760b
Robert Seidler <Robert.Seidler1@googlemail.com>
2013-04-26 20:26:28 +0200

    And that should delete the last of the IllnessReport-code.

--- src/main/java/de/unileipzig/xman/resources/deleteXman.sql
+++ src/main/java/de/unileipzig/xman/resources/deleteXman.sql
@@ -4,8 +4,6 @@ DROP TABLE IF EXISTS `o_xman_comment`;
 DROP TABLE IF EXISTS `o_xman_commentEntry`;
 DROP TABLE IF EXISTS `o_xman_esf`;
 DROP TABLE IF EXISTS `o_xman_exam`;
-DROP TABLE IF EXISTS `o_xman_illnessReport`;
-DROP TABLE IF EXISTS `o_xman_illnessReportEntry`;
 DROP TABLE IF EXISTS `o_xman_module`;
 DROP TABLE IF EXISTS `o_xman_protocol`;
 DROP TABLE IF EXISTS `o_xman_studyPath`;
