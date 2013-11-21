de292631930c605be7ef1f079b7591cd3395aaba
Klemens Schölhorn <klemens@schoelhorn.eu>
2013-11-21 05:38:19 +0100

    Fix MailManager after update to OO 9.1.1

--- src/main/java/de/unileipzig/xman/admin/mail/MailManager.java
+++ src/main/java/de/unileipzig/xman/admin/mail/MailManager.java
@@ -1,76 +1,30 @@
 package de.unileipzig.xman.admin.mail;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.velocity.VelocityContext;
+import org.olat.core.CoreSpringFactory;
 import org.olat.core.id.Identity;
-import org.olat.core.util.mail.MailTemplate;
-import org.olat.core.util.mail.MailerResult;
-import org.olat.core.util.mail.MailerWithTemplate;
+import org.olat.core.util.mail.MailBundle;
+import org.springframework.beans.factory.annotation.Autowired;
 
-/**
- * 
- * @author
- */
 public class MailManager {
-		
-	private static MailManager INSTANCE = null;
-
-	private MailManager() {
-		// singleton
+	@Autowired
+	org.olat.core.util.mail.MailManager mailer;
+	
+	public static MailManager getInstance() {
+		return CoreSpringFactory.getImpl(MailManager.class);
 	}
-	
-	static { INSTANCE = new MailManager(); }
-	
-	/**
-	 * @return Singleton.
-	 */
-	public static MailManager getInstance() { return INSTANCE; }
 
-	/**
-	 * this methode creates a new email template to the given body and subject.
-	 * 
-	 * @param body the body of the email
-	 * @param subject the subject of the mail
-	 * @return the mailTemplate
-	 */
-	public MailTemplate createEmail(String body, String subject, final Map<String, String> variables) {
-		
-		MailTemplate mailTemplate = new MailTemplate(subject, body, null) {
-			@Override
-			public void putVariablesInMailContext(VelocityContext context, Identity identity) {
-				
-//				String[] keys = (String[]) variables.keySet().toArray();
-//				
-//				for (int i = 0; i < variables.size(); i++ ) {
-//					
-//					context.put(keys[i], variables.get(i));
-//				}
-			}
-		};
-		return mailTemplate;
-	}
-	
 	/**
 	 * This method sends an email with the given subject and body to the choosen identity.
-	 * for the translator use this: {1} exam name {2} first name {3} app.date {4} app.place {5} app.duration {6} exam.typ  
 	 * @param body the body of the email
 	 * @param subject the subject of the email
 	 * @param identity recipient
 	 * @return true if the mail was successfully delivered to the recipients
 	 */
 	public boolean sendEmail(String subject, String body, Identity identity) {
+		MailBundle mail = new MailBundle();
+		mail.setToId(identity);
+		mail.setContent(subject, body);
 		
-		MailerResult result = null;
-		if ( identity != null) {
-			result = MailerWithTemplate.getInstance().sendRealMail(identity,this.createEmail(body, subject, new HashMap<String,String>()));
-		} else return false;
-
-		if ( result.getReturnCode() == MailerResult.OK ) {
-			return true;
-		} 
-		else return false;
+		return mailer.sendExternMessage(mail, null).isSuccessful();
 	}
-	
 }
