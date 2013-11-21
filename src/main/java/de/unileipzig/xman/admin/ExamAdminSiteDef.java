972e0c2393c0bc822261d4e001e22aafbd986df7
Klemens Schölhorn <klemens@schoelhorn.eu>
2013-11-21 05:39:07 +0100

    Rework admin site factory and position tab on the right
    The button was displayed on the left because of changed order values in OO.

--- src/main/java/de/unileipzig/xman/admin/ExamAdminSiteDef.java
+++ src/main/java/de/unileipzig/xman/admin/ExamAdminSiteDef.java
@@ -1,32 +1,15 @@
 package de.unileipzig.xman.admin;
 
-import java.util.List;
-
-import org.olat.core.extensions.ExtensionResource;
-// import org.olat.core.extensions.OLATExtension;
 import org.olat.core.gui.UserRequest;
 import org.olat.core.gui.control.WindowControl;
 import org.olat.core.gui.control.navigation.SiteDefinition;
 import org.olat.core.gui.control.navigation.SiteInstance;
 
 /**
- * 
- * @author
+ * This class represents the factory for the top menu sites.
+ * It's bean it loaded automatically by openolat.
  */
-public class ExamAdminSiteDef implements SiteDefinition { // alt: implements also OLATExtension
-	
-	/**
-	 * constructor
-	 */
-	public ExamAdminSiteDef(){
-		super();
-	}
-
-	/**
-	 * creates the new site definition
-	 * @param ureq the UserRequest
-	 * @param arg1 the window control
-	 */
+public class ExamAdminSiteDef implements SiteDefinition {
 	public SiteInstance createSite(UserRequest ureq, WindowControl arg1) {
 		SiteInstance si = null;
 		if ( ureq.getUserSession().getRoles().isInstitutionalResourceManager() ||  ureq.getUserSession().getRoles().isOLATAdmin()) {
@@ -36,50 +19,13 @@ public class ExamAdminSiteDef implements SiteDefinition { // alt: implements als
 		return si;
 	}
 
-	/**
-	 * nothing to do here
-	 * @return null
-	 */
-	public ExtensionResource getExtensionCSS() {
-		return null;
-	}
-
-	/**
-	 * nothing to do here
-	 * @return null
-	 */
-	public List getExtensionResources() {
-		return null;
-	}
-
-	/**
-	 * gets the name
-	 * @return "examAdminSite" (String)
-	 */
-	public String getName() {
-		return "examAdminSite";
-	}
-
-	/**
-	 * nothing to do here
-	 */
-	public void setup() {}
-
-	/**
-	 * nothing to do here
-	 */
-	public void tearDown() {}
-
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public int getOrder() {
-		// TODO Auto-generated method stub
-		return 1;
+		return 100;
 	}
-
 }
