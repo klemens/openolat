c822a4554e3a90067efea34de5078979a572cf86
Klemens Schölhorn <klemens@schoelhorn.eu>
2017-05-11 20:36:10 +0200

    Replace hilo sequence generation with enhanced-sequence using legacy-hilo
    The hilo generator has been removed in hibernate 5, see [1] for more details.
    
    [1]: 4aa5d4529fcfc00e8c962a6070dcd1dd7a39f467

--- src/main/java/de/unileipzig/xman/protocol/archived/ArchivedProtocol.java
+++ src/main/java/de/unileipzig/xman/protocol/archived/ArchivedProtocol.java
@@ -13,6 +13,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 import org.olat.core.id.Persistable;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
@@ -38,7 +39,14 @@ public class ArchivedProtocol implements Persistable {
 
 	@Id
 	@GeneratedValue(generator = "system-uuid")
-	@GenericGenerator(name = "system-uuid", strategy = "hilo")
+	@GenericGenerator(name = "system-uuid", strategy = "enhanced-sequence", parameters={
+		@Parameter(name="sequence_name", value="hibernate_unique_key"),
+		@Parameter(name="force_table_use", value="true"),
+		@Parameter(name="optimizer", value="legacy-hilo"),
+		@Parameter(name="value_column", value="next_hi"),
+		@Parameter(name="increment_size", value="32767"),
+		@Parameter(name="initial_value", value="32767")
+	})
 	@Column(name="id")
 	@XStreamOmitField
 	private Long key;
